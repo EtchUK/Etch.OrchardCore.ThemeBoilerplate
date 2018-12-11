5ac3db3c554d7d1bdd68efc79747dbc8a47ecb07
Peter Keating <peter@moov2.com>
2018-12-11 15:50:47 +0000

    Create empty OrchardCore theme
    - Create empty class library project target .NET Core 2.2
    - Add reference to OrchardCore.Theme.Targets &
    OrchardCore.DisplayManagement
    - Add theme Manifest
    - Add NuGet config that adds OrchardCore preview feed
    - Add basic Layout.cshtml & view imports

---
+++ Manifest.cs
@@ -0,0 +1,9 @@
+using OrchardCore.DisplayManagement.Manifest;
+
+[assembly: Theme(
+    Name = "Boilerplate Theme",
+    Author = "Moov2",
+    Website = "https://moov2.com",
+    Version = "0.0.1",
+    Description = "Boilerplate theme used as starting point for bespoke themes."
+)]
