e1a5b93c9348f494c2c8c7372ac68b317b73b7a9
Mobin <mobinzk@gmail.com>
2019-03-25 08:44:10 +0000

    Add improvements to webpack to build sass files separate from js (#2)
    * Add improvements to webpack to build sass files separate from js
    
    - Used `css-loader?-url` to not check for file path
    - Used FixStyleOnlyEntriesPlugin to remove the style.js artifact
    - Removed style-loader as we don’t load style using js anymore
    - Change JavaScript asset to `scripts.js`

--- content/Assets/Scripts/index.js
+++ content/Assets/Scripts/index.js
@@ -1,7 +1,1 @@
-/**
- * Import main Sass file so it is compiled by webpack in to
- * CSS file that can be referenced within `Views/Layout.cshtml`.
- */
-import '../Styles/index.scss';
-
 // entry point for theme JavaScript
