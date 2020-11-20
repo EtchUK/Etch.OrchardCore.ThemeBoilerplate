0e1a8c67152cf97a5ab29d5bb2ac3f036245e959
Peter Keating <pete.keating@etchuk.com>
2020-11-20 22:02:06 +0000

    Add JS component that sets `--scrollbarWidth` CSS variable
    JS component sets `--scrollbarWidth` CSS varaible to allow developers to
    account for scrollbar width when calculating 100vw "breakout" components
    which extend beyond their parent width. Use new `--scrollbarWidth`
    variable for calculating margins/widths for `align` & `flow` components.
    
    Resolves #174.

---
+++ content/Assets/Scripts/components/scrollbarWidth/index.ts
@@ -0,0 +1,21 @@
+/**
+ * Makes the scrollbar width available as a CSS variable.
+ *
+ * vw units return viewport width but assumes that scrollbars do not exist:
+ * https://drafts.csswg.org/css-values-3/#viewport-relative-lengths
+ * Scrollbar width changes per device, and is required if you want to
+ * accurately create a 100vw element (inside a thinner element) which
+ * accounts for the presence of a scrollbar.
+ *
+ * Example usage:
+ * `width: calc(100vw - var(--scrollbarWidth));`
+ */
+
+const scrollbarWidth = () => {
+    document.documentElement.style.setProperty(
+        '--scrollbarWidth',
+        `${window.innerWidth - document.documentElement.clientWidth}px`
+    );
+};
+
+export default scrollbarWidth;
