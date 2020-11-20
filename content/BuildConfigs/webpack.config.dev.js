421e20e3987da1205fda6a3f891036966c24a8a9
Peter Keating <pete.keating@etchuk.com>
2020-11-20 21:50:01 +0000

    Reduce size of build artifact
    - Ensure any development files are excluded from build artifact
    - No longer include pattern library files (html & content)
    
    Resolves #176.

--- content/BuildConfigs/webpack.config.dev.js
+++ content/BuildConfigs/webpack.config.dev.js
@@ -1,6 +1,3 @@
-const path = require('path');
-var fs = require('fs');
-
 const config = require('./webpack.config');
 
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
@@ -10,41 +7,5 @@ config.plugins.push(
         filename: '../css/[name].css',
     })
 );
-
-function replaceInFile(chunks, file) {
-    var filePath = path.join(__dirname, file);
-    var contents = fs.readFileSync(filePath, 'utf8');
-
-    var contentsOutput = contents;
-
-    for (var i = 0; i < chunks.length; i++) {
-        contentsOutput = contentsOutput.replace(
-            new RegExp('/' + chunks[i].names[0] + '..*.js', 'gi'),
-            '/' + chunks[i].names[0] + '.js'
-        );
-        contentsOutput = contentsOutput.replace(
-            new RegExp('/' + chunks[i].names[0] + '..*.css', 'gi'),
-            '/' + chunks[i].names[0] + '.css'
-        );
-    }
-
-    fs.writeFileSync(filePath, contentsOutput);
-}
-
-// Plugin to replace cache busted versions of
-// JS and CSS outputs with their non-cache busted
-// versions in case prod has overwritten out Layout.liquid
-config.plugins.push(function() {
-    this.plugin('done', function(statsData) {
-        var stats = statsData.toJson();
-
-        if (!stats.errors.length) {
-            replaceInFile(
-                stats.chunks,
-                '../PatternLibrary/patterns/_preview.hbs'
-            );
-        }
-    });
-});
 
 module.exports = config;
--- content/BuildConfigs/webpack.config.prod.js
+++ content/BuildConfigs/webpack.config.prod.js
@@ -1,5 +1,4 @@
 const path = require('path');
-var fs = require('fs');
 
 const config = require('./webpack.config');
 
@@ -20,45 +19,9 @@ config.plugins.push(
             path.join(process.cwd(), 'wwwroot/fonts'),
             path.join(process.cwd(), 'wwwroot/img'),
             path.join(process.cwd(), 'wwwroot/js'),
-            path.join(process.cwd(), 'wwwroot/patterns'),
             path.join(process.cwd(), 'wwwroot/Theme.png'),
         ],
-    }),
-)
-
-function replaceInFile(chunks, file) {
-    var filePath = path.join(__dirname, file);
-    var contents = fs.readFileSync(filePath, 'utf8');
-
-    var contentsOutput = contents;
-
-    for (var i = 0; i < chunks.length; i++) {
-        contentsOutput = contentsOutput.replace(
-            new RegExp('/' + chunks[i].names[0] + '.js', 'gi'),
-            '/' + chunks[i].names[0] + '.js?v=' + chunks[i].hash
-        );
-        contentsOutput = contentsOutput.replace(
-            new RegExp('/' + chunks[i].names[0] + '.css', 'gi'),
-            '/' + chunks[i].names[0] + '.css?v=' + chunks[i].hash
-        );
-    }
-
-    fs.writeFileSync(filePath, contentsOutput);
-}
-
-// Plugin to replace non-cache busted versions of
-// JS and CSS outputs with their cache busted versions
-config.plugins.push(function () {
-    this.plugin('done', function (statsData) {
-        var stats = statsData.toJson();
-
-        if (!stats.errors.length) {
-            replaceInFile(
-                stats.chunks,
-                '../PatternLibrary/patterns/_preview.hbs'
-            );
-        }
-    });
-});
+    })
+);
 
 module.exports = config;
