4cce7d0fb14e4f5f14e22b701f6b9d626883fe45
Peter Keating <pete.keating@etchuk.com>
2021-09-05 22:27:10 +0100

    v1.1.0

--- content/Manifest.cs
+++ content/Manifest.cs
@@ -4,6 +4,6 @@ using OrchardCore.DisplayManagement.Manifest;
     Name = "Boilerplate Theme",
     Author = "Etch UK",
     Website = "https://etchuk.com",
-    Version = "1.0.0-rc2",
+    Version = "1.1.0",
     Description = "Theme boilerplate is our starting point for building Orchard Core themes."
 )]
