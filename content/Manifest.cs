4a91509223787848b77fdceb991ef24422af5784
Peter Keating <pete.keating@etchuk.com>
2021-11-01 12:11:28 +0000

    Upgrade OrchardCore to 1.1.0

--- content/Manifest.cs
+++ content/Manifest.cs
@@ -4,6 +4,6 @@ using OrchardCore.DisplayManagement.Manifest;
     Name = "Boilerplate Theme",
     Author = "Etch UK",
     Website = "https://etchuk.com",
-    Version = "1.2.1",
+    Version = "1.3.0",
     Description = "Theme boilerplate is our starting point for building Orchard Core themes."
 )]
