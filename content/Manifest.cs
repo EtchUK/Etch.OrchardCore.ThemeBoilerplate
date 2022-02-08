87dced3d0457aa9864fb1183aab25577c730e5c3
Adam Burt <adam.burt@etchuk.com>
2022-02-08 10:14:35 +0000

    v1.3.3

--- content/Manifest.cs
+++ content/Manifest.cs
@@ -4,6 +4,6 @@ using OrchardCore.DisplayManagement.Manifest;
     Name = "Boilerplate Theme",
     Author = "Etch UK",
     Website = "https://etchuk.com",
-    Version = "1.3.2",
+    Version = "1.3.3",
     Description = "Theme boilerplate is our starting point for building Orchard Core themes."
 )]
