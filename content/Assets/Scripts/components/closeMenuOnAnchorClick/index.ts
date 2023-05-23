197144e924bd9b72da24bbe9c5633a288042b8d1
Adam Burt <adam.burt@etchuk.com>
2023-05-23 10:16:43 +0100

    Close menu on anchor link click
    To ensure scrolled-to content is visible on mobile, such as when anchor link appears in primary nav, or other links that could appear in the header while the menu is open

---
+++ content/Assets/Scripts/components/closeMenuOnAnchorClick/index.ts
@@ -0,0 +1,16 @@
+const ANCHOR_LINK_SELECTOR = 'a[href*="#"]';
+const CSS_VISIBLE = 'is-nav-visible';
+
+const closeMenuOnAnchorClick = () => {
+    const anchorLinks = document.querySelectorAll(ANCHOR_LINK_SELECTOR);
+    const $html = document.querySelector('html') as HTMLHtmlElement;
+
+    anchorLinks.forEach((link) => {
+        link.addEventListener('click', (e) => {
+            $html.classList.remove(CSS_VISIBLE);
+            return true;
+        });
+    });
+};
+
+export default closeMenuOnAnchorClick;
