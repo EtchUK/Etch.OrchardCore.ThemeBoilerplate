8fd4c8c307c384c60cf06b815ed39f7ab527cdfd
Peter Keating <pete.keating@etchuk.com>
2023-07-14 11:09:07 +0100

    TypeScript build improvements
    - Run `tsc` as part of build to ensure TypeScript errors are caught
    - Run `tsc` as a pre-commit hook to ensure TypeScript errors are caught
    before commit can be completed
    - Use same `tsconfig.json` as used on Etch CMS
    - Fix typescript compile errors

--- content/Assets/Scripts/components/closeMenuOnAnchorClick/index.ts
+++ content/Assets/Scripts/components/closeMenuOnAnchorClick/index.ts
@@ -1,12 +1,12 @@
 const ANCHOR_LINK_SELECTOR = 'a[href*="#"]';
-const CSS_VISIBLE = 'is-nav-visible';
+const CSS_VISIBLE = "is-nav-visible";
 
 const closeMenuOnAnchorClick = () => {
     const anchorLinks = document.querySelectorAll(ANCHOR_LINK_SELECTOR);
-    const $html = document.querySelector('html') as HTMLHtmlElement;
+    const $html = document.querySelector("html") as HTMLHtmlElement;
 
     anchorLinks.forEach((link) => {
-        link.addEventListener('click', (e) => {
+        link.addEventListener("click", () => {
             $html.classList.remove(CSS_VISIBLE);
             return true;
         });
